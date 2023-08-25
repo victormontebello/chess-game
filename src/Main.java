81d3f2e16aac7c9412dd9cb9f3a35239d90cb59a
Victor Montebello <montebellovictor@gmail.com>
2023-08-25 18:57:08 -0300

    Chess Match n print board

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import chess.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
