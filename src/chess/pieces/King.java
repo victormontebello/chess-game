2a6677a6d03f54cc99360e57187e84552b51689c
Victor Montebello <montebellovictor@gmail.com>
2023-08-27 19:09:08 -0300

    King moves

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -11,9 +11,48 @@ public class King extends ChessPiece{
         return "K";
     }
 
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece)getBoard().piece(position); // p = piece
+        return p == null || p.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()]; // mat = matrix
+        Position pos = new Position(0, 0);
+        // above
+        pos.setValues(position.getRow() - 1, position.getColumn());
+        if (getBoard().positionExists(pos) && canMove(pos)) // if position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // below
+        pos.setValues(position.getRow() + 1, position.getColumn());
+        if (getBoard().positionExists(pos) && canMove(pos)) // if position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // left
+        pos.setValues(position.getRow(), position.getColumn() - 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) // if position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // right
+        pos.setValues(position.getRow(), position.getColumn() + 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) // if position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // nw
+        pos.setValues(position.getRow() - 1, position.getColumn() - 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) // if position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // ne
+        pos.setValues(position.getRow() - 1, position.getColumn() + 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) // if position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // sw
+        pos.setValues(position.getRow() + 1, position.getColumn() - 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) // if position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // se
+        pos.setValues(position.getRow() + 1, position.getColumn() + 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) // if position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+
         return mat;
     }
 }
