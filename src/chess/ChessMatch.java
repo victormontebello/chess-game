d8b1fca27274d8148c6b911c3441569f96517f23
Victor Montebello <montebellovictor@gmail.com>
2023-08-27 12:52:23 -0300

    Rook moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -21,9 +21,16 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
+    private void validateTargetPosition(Position source, Position target) {
+        if(!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to target position");
+        }
+    }
+
     private void validateSourcePosition(Position position) {
         if(!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
@@ -39,8 +46,6 @@ public class ChessMatch {
         return capturedPiece;
     }
 
-
-
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
@@ -48,10 +53,16 @@ public class ChessMatch {
     private void initialSetup() {
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 2, new King(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 1, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 1, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 
     }
 
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -14,6 +14,40 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()]; // mat = matrix
+        Position pos = new Position(0, 0);
+        // above
+        pos.setValues(position.getRow() - 1, position.getColumn());
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) { // while position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+            pos.setRow(pos.getRow() - 1);
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) // if position exists and there is an opponent piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // left
+        pos.setValues(position.getRow(), position.getColumn() - 1);
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) { // while position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+            pos.setColumn(pos.getColumn() - 1);
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) // if position exists and there is an opponent piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // right
+        pos.setValues(position.getRow(), position.getColumn() + 1);
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) { // while position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+            pos.setColumn(pos.getColumn() + 1);
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) // if position exists and there is an opponent piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+        // below
+        pos.setValues(position.getRow() + 1, position.getColumn());
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) { // while position exists and there is no piece
+            mat[pos.getRow()][pos.getColumn()] = true;
+            pos.setRow(pos.getRow() + 1);
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) // if position exists and there is an opponent piece
+            mat[pos.getRow()][pos.getColumn()] = true;
         return mat;
     }
+
 }
